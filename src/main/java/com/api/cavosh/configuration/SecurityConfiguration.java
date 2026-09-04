969116841a2f4b8da9ab2cf98bd68c2459179157
crodrigoDev <212727257+crodrigoDev@users.noreply.github.com>
2026-09-03 23:48:06 -0500

    feat(auth): configurar rutas púulicas de autenticaciÃon

--- src/main/java/com/api/cavosh/configuration/SecurityConfiguration.java
+++ src/main/java/com/api/cavosh/configuration/SecurityConfiguration.java
@@ -2,11 +2,16 @@ package com.api.cavosh.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
- * Configura componentes compartidos relacionados con seguridad
+ * Configura componentes y reglas compartidas de seguridad de la aplicacion
  */
 @Configuration
 public class SecurityConfiguration {
@@ -19,5 +24,42 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+
+    /**
+     * Configura la cadena de filtros de seguridad de la API
+     *
+     * @param http configuracion HTTP de Spring Security
+     * @return cadena de filtros configurada
+     * @throws Exception si Spring Security no puede construit la cadena de filtros
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http
+    ) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                HttpMethod.POST,
+                                "/api/v1/auth/registrar"
+                        ).permitAll()
+                        .requestMatchers(
+                                HttpMethod.POST,
+                                "/api/v1/auth/login"
+                        ).permitAll()
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .build();
     }
 }
